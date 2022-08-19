92bdf0b7d5afc1566e797e382376d518f4878473
SEIMIN123 <interkp12@gmail.com>
2022-08-19 16:17:44 +0900

    변수

---
+++ python-variable(변수).py
@@ -0,0 +1,18 @@
+# price = 10000
+# vat_rate = 0.1
+# vat = price*vat_rate
+# print(vat);
+
+from email import message
+from unicodedata import name
+
+
+print('hi, sori....bye, sori.');
+
+name = 'seimin';
+message = 'hi, '+name+' .... bye, '+name+'.';
+print('hi, '+name+' .... bye, '+name+'.');
+print(message);
+
+
+# todo : 변수랑 같음.변수안에 넣어두고 프린트하면 출력됨.자바스크립트처럼 const 안쓰고 그냥 됨 자바스크립트도 됨. 걍 시발 똑같음.
